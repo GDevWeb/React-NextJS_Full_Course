ae7e49f7ed076f44074a09bccb82037dbbddc24e
GDevWeb <zodiaquestar@gmail.com>
2024-08-28 20:49:30 +0200

    37. Handling Side Effects with useEffect()

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -4,10 +4,13 @@
   30.Adding a Shared Header & more State Management
     32. Handling Form Submission
     Error has occurred with the basic code for the modal "hard coded" , i have to use <modal> from react-dom 
-34. Outputting List Data
+  34. Outputting List Data
+  35. Adding a Backend to the React SPA
+  36. Sending a POST HTTP Request
+  37. Handling Side Effects with useEffect()
 
  */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "./Modal";
 import NewPost from "./NewPost";
 import Post from "./Post";
@@ -25,11 +28,29 @@ interface Post {
 
 function PostsList({ isPosting, onStopPosting }: PostsListProps) {
   // 1.***State***
+  // Get all :
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("http://localhost:8080/posts");
+      const data = await response.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
+
   const [posts, setPosts] = useState<Post[]>([]);
   // 2.***Functions***
 
   const addPostHandler = (postData: Post) => {
     setPosts((existingPosts) => [postData, ...existingPosts]);
+    // Post :
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   };
 
   const getDate = () => {
